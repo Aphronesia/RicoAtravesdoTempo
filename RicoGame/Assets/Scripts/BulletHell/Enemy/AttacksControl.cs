800018edd06052848e2f1e257d92b5e8f816bc49
Ferle <frmoura15@gmail.com>
2025-03-21 12:36:37 -0300

    bulletHell attacks

---
+++ RicoGame/Assets/Scripts/BulletHell/Enemy/AttacksControl.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttacksControl : MonoBehaviour
+{
+    [SerializeField]
+    private EnemyAttack[] enemyAttack;
+}
