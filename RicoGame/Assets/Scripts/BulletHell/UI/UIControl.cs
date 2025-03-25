97caf22b7666b8ef703c1fa91e11da49c841fb8b
Ferle <frmoura15@gmail.com>
2025-03-25 12:35:08 -0300

    BH UI System
    menu

--- RicoGame/Assets/Scripts/BulletHell/UI/UIControl.cs
+++ RicoGame/Assets/Scripts/BulletHell/UI/UIControl.cs
@@ -8,7 +8,7 @@ public class UIControl : MonoBehaviour
 {
     private ControlScenes controlScenes;
     [SerializeField]
-    private GameObject uiStart, uiPause;
+    private GameObject CtrlScenes, uiStart, uiPause, uiDead;
     public bool running, paused;
     [SerializeField]
     private Sprite atkBttnOn, atkBttnOff;
@@ -17,9 +17,11 @@ public class UIControl : MonoBehaviour
     public static event Action<bool> OnStarted;
     private void OnEnable() {
         EnemyControl.OnEnemyTired += EnemyTired;
+        Player_Status.OnPlayeDeath += PlayerDie;
     }
     private void OnDisable() {
         EnemyControl.OnEnemyTired -= EnemyTired;
+        Player_Status.OnPlayeDeath -= PlayerDie;
     }
     private void EnemyTired(bool value){
         if (value){
@@ -29,14 +31,24 @@ public class UIControl : MonoBehaviour
             AtkButton.sprite = atkBttnOff;
         }
     }
-    private void Start() {
+    private void GetUIObjects(){
         uiStart = GameObject.Find("Canvas/UIStart");
-        GameObject CtrlScenes = GameObject.Find("ScenesController");
-        if (CtrlScenes != null){
-            controlScenes = CtrlScenes.GetComponent<ControlScenes>();
-        }
+        uiPause = GameObject.Find("Canvas/UIPause");
+        uiDead = GameObject.Find("Canvas/UIDead");
+    }
+    private void Start() {
+        controlScenes = CtrlScenes.GetComponent<ControlScenes>();
+        GetUIObjects();
         if (uiStart != null){
             uiStart.SetActive(true);
+        }
+        if (uiPause != null){
+            uiPause.SetActive(false);
+            paused = false;
+            controlScenes.Pause(paused);
+        }
+        if (uiDead != null){
+            uiDead.SetActive(false);
         }
         running = false;
     }
@@ -46,13 +58,19 @@ public class UIControl : MonoBehaviour
         uiStart.SetActive(false);
         OnStarted?.Invoke(running);
     }
-    public void UIPause(){
+    public void PlayerDie(){
+        uiDead.SetActive(true);
+        controlScenes.Pause(true);
+    }
+    public void Pause(){
+        paused =! paused;
+        uiPause.SetActive(paused);
         controlScenes.Pause(paused);
     }
-    public void UIRestart(){
-
+    public void Restart(){
+        controlScenes.RestartGame();
     }
-    public void UIQuit(){
-
+    public void Quit(){
+        controlScenes.QuitGame();
     }
 }
