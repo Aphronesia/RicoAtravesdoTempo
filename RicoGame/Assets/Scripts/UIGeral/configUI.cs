b0d9dae7104941634aa7ae285b5eef78e4eb4235
Ferle <frmoura15@gmail.com>
2025-05-05 12:32:03 -0300

    nao gostei

--- RicoGame/Assets/Scripts/UIGeral/configUI.cs
+++ RicoGame/Assets/Scripts/UIGeral/configUI.cs
@@ -7,8 +7,14 @@ public class configUI : MonoBehaviour
 {
     [SerializeField]
     private GameObject UIMenuPause;
+    private RectTransform _pauseRectTransform;
     [SerializeField]
-    private bool pause;
+    private Vector3 outPosition;
+
+
+
+    [SerializeField]
+    private bool pause = false;
     private ControlScenes _controlScenes;
     public static event Action<bool> OnPause;
     
@@ -20,7 +26,9 @@ public class configUI : MonoBehaviour
     }
     private void Start() {
         UIMenuPause = GameObject.Find("UIMenuPause");
-        
+        if(UIMenuPause != null){
+            _pauseRectTransform = UIMenuPause.GetComponent<RectTransform>()
+;        }
         ControlScenes ScenesController = FindObjectOfType<ControlScenes>();
         if (ScenesController != null){
             _controlScenes = ScenesController.GetComponent<ControlScenes>();
@@ -29,5 +37,16 @@ public class configUI : MonoBehaviour
     public void Pause(){
         pause =! pause;
         OnPause?.Invoke(pause);
+
+        if(pause){
+            _pauseRectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            _pauseRectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+
+            _pauseRectTransform.pivot = new Vector2(0.5f, 0.5f);
+            _pauseRectTransform.anchoredPosition = Vector2.zero;
+        }
+        else{
+            _pauseRectTransform.pivot = outPosition;
+        }
     }
 }
