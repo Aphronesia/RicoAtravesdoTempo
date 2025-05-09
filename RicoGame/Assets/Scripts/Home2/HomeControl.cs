23589a4793c28854e51604089de8667c46543f49
Ferle <frmoura15@gmail.com>
2025-05-09 10:02:09 -0300

    home

---
+++ RicoGame/Assets/Scripts/Home2/HomeControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeControl : MonoBehaviour
+{
+    public ControlScenes controlScenes;
+    private Game.SaveLoadSystem saveLoadSystem;
+    private void Start() {
+        ControlScenes ScenesController = FindObjectOfType<ControlScenes>();
+        if (ScenesController != null){
+            controlScenes = ScenesController.GetComponent<ControlScenes>();
+        }
+        Game.SaveLoadSystem saveLoader = FindObjectOfType<Game.SaveLoadSystem>();
+        if (saveLoader != null){
+            saveLoadSystem = saveLoader.GetComponent<Game.SaveLoadSystem>();
+        }
+        //controla rotacao de tela (muda pra retrato)
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+    private void StartNewGame(){
+
+    }
+}
