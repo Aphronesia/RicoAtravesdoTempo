96ff7d8a40e605daa86f3258cfa1189afa6d04f0
Ferle <frmoura15@gmail.com>
2025-08-15 10:42:51 -0300

    dada

---
+++ RicoGame/Assets/Scripts/BulletHell/Enemy/Attacks/ClawScratch.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace BulletHell.Enemy.Attacks
+{
+    public class ClawScratch : MonoBehaviour
+    {
+        [SerializeField] private Transform claw;
+        [SerializeField] private Transform scratch;
+    
+    }
+}
